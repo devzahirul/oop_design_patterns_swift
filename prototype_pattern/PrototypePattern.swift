c71a6bdd9b071eb6f95d54c998b6dda3be5487ca
Zahirul Islam <jislam150@gmail.com>
2020-02-14 08:56:42 +0600

    Create PrototypePattern.swift

---
+++ prototype_pattern/PrototypePattern.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+//Simple implementation for Prototype pattern
+protocol Prototype {
+    var name: String {set get}
+    func clone() -> Prototype
+}
+
+final class ConcretePrototypel: Prototype {
+    var name: String = "ConcretePrototype1"
+    
+    func clone() -> Prototype {
+        return ConcretePrototypel.init()
+    }
+}
+
+final class ConcretePrototype2: Prototype {
+    
+    var name: String = "ConcretePrototype2"
+    
+    
+    func clone() -> Prototype {
+        return ConcretePrototype2.init()
+    }
+}
+
+//Test
+var prototype1 = ConcretePrototypel.init()
+//Create 2 prototype
+var prototype2 = prototype1.clone()
+var prototype3 = prototype1.clone()
+
+print(prototype2.name) //Output: ConcretePrototype1
+prototype2.name = "ChangeBy2" //Change name of prototype2
+print(prototype2.name) //Output: ChangeBy2
+//not change the name of prototype3
+print(prototype3.name) //Output: ConcretePrototype1
