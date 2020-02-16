23812dd9f1beca5048c7f8637cca44e1e95808cb
Islam_MD_Zahirul <jislam150@gmail.com>
2020-02-16 15:43:56 +0600

    MultiThread Flyweight pattern add

---
+++ multithread_flyweight_pattern/FlyweightPattern.playground/Contents.swift
@@ -0,0 +1,90 @@
+import UIKit
+
+//A simple example of Flyweight pattern
+
+final class CoffeeFlavour: Hashable {
+    static func == (lhs: CoffeeFlavour, rhs: CoffeeFlavour) -> Bool {
+        return lhs.flavour == rhs.flavour
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(flavour)
+    }
+    
+    var flavour: String
+    
+    init(flavour: String) {
+        self.flavour = flavour
+    }
+}
+
+protocol ICoffeeFlavourFactory {
+    func getFlavour(flavour: String, result: @escaping (CoffeeFlavour) -> Void)
+}
+
+final class ReduceMemoryFootprint: ICoffeeFlavourFactory {
+    
+    private let _lock = NSLock.init()
+    private var _cache = [String: CoffeeFlavour]()
+    
+    private let queue = DispatchQueue.init(label: "queueforflavourcache",
+                                           qos: .userInteractive)
+    
+    func getFlavour(flavour: String,result: @escaping (CoffeeFlavour) -> Void) {
+        queue.async {[unowned self] in
+            
+            
+           // print("Before \(flavour) : \(self._cache[flavour])")
+            
+            if let cacheFlavour = self._cache[flavour] {
+                print("return from cache")
+                result(cacheFlavour)
+            } else {
+                let newFlavour = CoffeeFlavour.init(flavour: flavour)
+                self._cache[flavour] = newFlavour
+                print("Created new")
+                result(newFlavour)
+            }
+            
+        }
+    }
+}
+
+
+let flavourStore = ReduceMemoryFootprint.init()
+
+//let flavour1 = flavourStore.getFlavour(flavour: "Flavour_type_1")
+
+DispatchQueue.global().async {
+    flavourStore.getFlavour(flavour: "Flavour_type_2",result: { coffee in
+        //printcoffee.flavour
+    })
+}
+
+DispatchQueue.global().async {
+    flavourStore.getFlavour(flavour: "Flavour_type_1",result: {coffee in
+        
+    })
+}
+
+DispatchQueue.global().async {
+    flavourStore.getFlavour(flavour: "Flavour_type_2"){coffee in
+        
+    }
+}
+
+DispatchQueue.global().async {
+    flavourStore.getFlavour(flavour: "Flavour_type_1",result: {coffee in
+        
+    })
+}
+
+
+//Output:
+/*
+ Created new
+ Created new
+ return from cache
+ return from cache
+ */
+
